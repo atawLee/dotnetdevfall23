dab15cbeaa277bb006ce1aa294dd38618e6d4bbc
jonghoon <jonghoon.1738@gmail.com>
2023-09-28 17:36:50 +0900

    테스트 힘든 코드 예시

---
+++ WebTest/테스트힘든코드.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mesWeb.Database.Entity;
+using mesWeb.Repository;
+using mesWeb.Service;
+using Moq;
+
+namespace WebTest
+{
+    public class 테스트힘든코드
+    {
+        [Fact]
+        public void 생산시작()
+        {
+            //준비코드 
+            //suit
+            var testSuit = new UnTestableMesService();
+
+            //case
+            testSuit.AddManufacture(0, "manufactureSummary", "Lee");
+        }
+    }
+}
