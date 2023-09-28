8cff64c8428219478d537c05df3720137fa87ae7
jonghoon <jonghoon.1738@gmail.com>
2023-09-28 15:27:06 +0900

    db 구조개선

---
+++ mesWeb/Database/Entity/Manufacture.cs
@@ -0,0 +1,20 @@
+﻿namespace mesWeb.Database.Entity;
+
+public class Manufacture
+{
+    public int Id { get; set; }
+    public string Summary { get; set; }
+    
+    public string Worker { get; set; }
+
+    public DateTime WorkStartTime { get; set; } = DateTime.Now;
+
+    public DateTime? WorkEndTime { get; set; }
+
+    public decimal WorkQty { get; set; }
+    public decimal FaultQty { get; set; }
+
+    // Foreign key
+    public int WorkOrderId { get; set; }
+    public WorkOrder WorkOrder { get; set; }
+}
--- mesWeb/Migrations/20230928062546_v092802.Designer.cs
+++ mesWeb/Migrations/20230928062546_v092802.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mesWeb.Entity;
 
@@ -10,9 +11,11 @@ using mesWeb.Entity;
 namespace mesWeb.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230928062546_v092802")]
+    partial class v092802
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
